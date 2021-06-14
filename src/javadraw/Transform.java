0614891d1bca4eebdb44d4def4e156b620c03c9f
Noah <noahcoder77@gmail.com>
2021-06-14 18:10:14 -0400

    Added accessors to Transform.

--- src/javadraw/Transform.java
+++ src/javadraw/Transform.java
@@ -11,4 +11,28 @@ public class Transform {
 
     protected double width, height, angle;
 
+    /**
+     * Get the width of the Transform
+     * @return a double representing the width
+     */
+    public double width() {
+        return this.width;
+    }
+
+    /**
+     * Get the height of the Transform
+     * @return a double representing the height
+     */
+    public double height() {
+        return this.height;
+    }
+
+    /**
+     * Get the angle/rotation of the Transform
+     * @return a double representing the angle
+     */
+    public double angle() {
+        return this.angle;
+    }
+
 }
